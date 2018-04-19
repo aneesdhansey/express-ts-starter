a120c6a4182b10842860b23e67e5bf9276eeb4e5
Anees Dhansey <aneesdhansey@gmail.com>
2018-04-19 12:01:12 +0530

    Removed build directory

--- build/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var env = process.env.NODE_ENV || 'development';
-if (env == 'development' || env == 'test') {
-    var config = require('./config.json');
-    var envConfig_1 = config[env];
-    Object.keys(envConfig_1).forEach(function (k) { return process.env[k] = envConfig_1[k]; });
-}
--- build/models/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_model_1 = require("./user.model");
-module.exports = { User: user_model_1.User };
